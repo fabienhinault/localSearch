229b941ab8dc9a58ed08a60a35f906ab8c95d7b5
Fabien Hinault <fhinault@sii.fr>
2016-10-28 18:15:48 +0200

    remove google test in addUrl.js

--- data/addUrl.js
+++ data/addUrl.js
@@ -1,6 +1,3 @@
 var strPageContent = document.body.innerHTML;
 var url = document.location.toString();
-//console.log(url);
-if (!(url.match("google"))) {
-  self.port.emit("innerHTML", {url: url, content: strPageContent});
-}
+self.port.emit("innerHTML", {url: url, content: strPageContent});
